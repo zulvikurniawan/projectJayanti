90ba596a07120465928c1b3c0ab379601f2f23f2
Zulvi Kurniawan <zulvikurniawan66@gmail.com>
2022-08-06 02:20:08 +0700

    menambahkan page test.php tanpa isi

---
+++ app/Views/pages/test.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<?= $this->endSection(); ?>
+
+//java skript
+<?= $this->section('javascript'); ?>
+
+<?= $this->endSection(); ?>
+// akhir java skript
